777457a00d53489f10e9a09f95f9f6880745cb73
Vinicius Kalach <viniciuscato@hotmail.com>
2023-09-08 17:25:37 -0300

    exercicios 3 e 4

--- Javascript_Exercícios/Aula01/Exercício3.js
+++ Javascript_Exercícios/Aula01/Exercício3.js
@@ -1,3 +1,8 @@
 /*
 3. Faça um programa que mostre a quantidade de caracteres para o texto “Disciplina de Programação para web”.
 */
+
+const texto = "Disciplina de Programação para web";
+const caracteres = texto.length;
+
+console.log(`A quantidade de caracteres no texto é: ${caracteres}`);
--- Javascript_Exercícios/Aula01/Exercício4.js
+++ Javascript_Exercícios/Aula01/Exercício4.js
@@ -1,3 +1,13 @@
 /*
 4. Faça um programa que recebe o nome de uma variável e mostre o <<primeiro>>.<<último nome>>@facens.br
 */
+
+const nome = "Vinicius Kalach Freitas Cato"
+const nomes = nome.split(' ') 
+
+const primeiroNome = nomes[0] 
+const ultimoNome = nomes[nomes.length -1]
+
+const email = `${primeiroNome}.${ultimoNome}@facens.br`
+console.log(email)
+
