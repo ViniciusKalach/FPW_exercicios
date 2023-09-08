4fdc08e03cb51d423b4d1ffc5e91bbd1e879657b
Vinicius Kalach <viniciuscato@hotmail.com>
2023-09-08 18:04:13 -0300

    exercícios 9, 10 e 11

--- Javascript_Exercícios/Aula01/Exercício8.js
+++ Javascript_Exercícios/Aula01/Exercício8.js
@@ -1,3 +1,11 @@
 /*
 8. Crie um objeto que represente uma pessoa, com propriedades como nome, idade e profissão. Imprima o objeto no console.
 */
+
+const pessoa = {
+    nome: "Vinícius",
+    idade: 19,
+    profissao: "Estagiário"
+  };
+  
+  console.log(pessoa);
--- Javascript_Exercícios/Aula01/Exercício9.js
+++ Javascript_Exercícios/Aula01/Exercício9.js
@@ -1,3 +1,16 @@
 /*
 9. Escreva uma função que receba uma string como argumento e retorne a string invertida.
 */
+
+function inverter(frase) {
+    const letras = frase.split('');
+    const letrasInvertidas = letras.reverse();
+    const fraseInvertida = letrasInvertidas.join('');
+    return fraseInvertida;
+  }
+
+  // Teste de aplicação
+  
+const frase = "Socorram-me, subi num onibus em Marrocos"
+console.log(frase) // Deverá imprimir a frase original
+console.log(inverter(frase)) // Deverá imprimir a frase inversa
