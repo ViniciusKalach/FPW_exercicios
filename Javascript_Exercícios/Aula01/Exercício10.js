485ef1acf4dcd5a8aac13800d058230939506294
Vinicius Kalach <viniciuscato@hotmail.com>
2023-09-09 14:21:14 -0300

    ex10 e 11 que não foi no commit anterior

--- Javascript_Exercícios/Aula01/Exercício10.js
+++ Javascript_Exercícios/Aula01/Exercício10.js
@@ -1,3 +1,25 @@
 /*
 10. Escreva uma função que recebe uma string como argumento e retorna o número de vogais presentes nessa string. Considere apenas vogais minúsculas (a, e, i, o, u).
 */
+
+function contarVogais(frase) {
+    const fraseMinuscula = frase.toLowerCase();
+  
+    let contadorVogais = 0;
+  
+    for (let i = 0; i < fraseMinuscula.length; i++) {
+      const caractere = fraseMinuscula[i];
+      
+      if (caractere === 'a' || caractere === 'e' || caractere === 'i' || caractere === 'o' || caractere === 'u') {
+        contadorVogais++;
+      }
+    }
+  
+    return contadorVogais;
+  }
+  
+  // Teste de aplicação
+
+  const fraseTeste = "Hello, world!";
+  const numeroDeVogais = contarVogais(fraseTeste);
+  console.log(`Número de vogais: ${numeroDeVogais}`);
--- Javascript_Exercícios/Aula01/Exercício11.js
+++ Javascript_Exercícios/Aula01/Exercício11.js
@@ -2,3 +2,16 @@
 11. Escreva uma função que recebe um endereço de e-mail como argumento e retorna true se o e-mail for válido ou false caso contrário. 
 Um e-mail válido deve conter um único símbolo '@' e pelo menos um ponto '.' após o '@'.
 */
+
+function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+    // Testa o e-mail com a expressão regular
+    return regex.test(email);
+  }
+  
+  // Teste de aplicação
+  
+  console.log(validarEmail("211559@facens.com")); // Deverá retornar true
+  console.log(validarEmail("211559.com")); // Deverá retornar false
+  console.log(validarEmail("211559@invalido")); // Deverá retornar false
